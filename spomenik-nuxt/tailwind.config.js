c19b7c41208038eaea75ddf5aa8b396778551b25
Milena Petkanova <milenapetkanova@gmail.com>
2022-02-16 14:19:59 +0200

    init tailwind styles

---
+++ spomenik-nuxt/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'purple-custom-600': '#6D22BD',
+        'purple-custom-400': '#A353CE',
+        'blue-custom-600': '#40B0D5',
+        'blue-custom-400': '#5DFFE8',
+      },
+      fontFamily: {
+        'montserrat': ['montserrat', 'sans-serif'],
+        'nunito': ['nunito', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
