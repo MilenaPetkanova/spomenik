81f5859bb9485f92d247be3dc8d871018bb1c0cd
Milena Petkanova <milenapetkanova@gmail.com>
2022-02-16 14:21:52 +0200

    create default layout

---
+++ spomenik-nuxt/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <main class="layout is-default font-montserrat bg-current">
+    <Header />
+    <Nuxt />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.layout {
+  min-height: 100vh;
+  height: 100%;
+}
+</style>
