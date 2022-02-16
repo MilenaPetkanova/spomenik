787238069f7df931cbaf954e29e4f7c5ee75a514
Milena Petkanova <milenapetkanova@gmail.com>
2022-02-16 14:21:26 +0200

    create header, logo and nav components

---
+++ spomenik-nuxt/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+	<header class="header">
+		<div class="header__container container flex justify-between py-8">
+			<Logo />
+			<Nav />
+		</div>
+	</header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+	&__container {
+		margin: 0 auto;
+	}
+}
+</style>
---
+++ spomenik-nuxt/components/Logo.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLink to="/">
+    <h1 class="logo w-8 h-8 text-blue-custom-400 font-bold">LOGO</h1>
+  </NuxtLink>
+</template>
+
+<style>
+.nuxt-logo {
+  height: 180px;
+}
+</style>
