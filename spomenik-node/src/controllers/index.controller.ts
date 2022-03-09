0b1eaca85375553f21bbf63517b0da89d13f8a03
Milena Petkanova <milenapetkanova@gmail.com>
2022-03-09 17:14:54 +0200

    create gallery crud endpoints

--- spomenik-node/src/controllers/index.controller.ts
+++ spomenik-node/src/controllers/index.controller.ts
@@ -2,6 +2,8 @@ import {Request, Response} from 'express'
 import {QueryResult} from 'pg'
 import pool from '../database'
 
+// Letters
+
 export const getLetters = async (req: Request, res: Response): Promise<Response> => {
 	try {
 		const response: QueryResult = await pool.query('SELECT * FROM letters ORDER BY id DESC')
@@ -55,6 +57,55 @@ export const deleteLetter = async (req: Request, res: Response): Promise<Respons
 		const id = parseInt(req.params.id);
 		await pool.query('DELETE FROM letters where id = $1', [id])
 		return res.status(200).json(`Letter ${id} deleted successfully`);		
+	} catch(e) {
+		console.error(e);
+		return res.status(500).json('Internal server error');
+	}
+}
+
+
+// Gallery
+
+export const getGallery = async (req: Request, res: Response): Promise<Response> => {
+	try {
+		const response: QueryResult = await pool.query('SELECT * FROM gallery ORDER BY id DESC')
+		return res.status(200).json(response.rows);
+	} catch(e) {
+		console.error(e);
+		return res.status(500).json('Internal  error');
+	}
+}
+
+export const createGalleryRecord = async (req: Request, res: Response): Promise<Response> => {
+	try {
+		const { src, caption } = req.body;
+		await pool.query(`INSERT INTO gallery (src, caption) VALUES ('${src}', '${caption}')`)
+		return res.status(201).json({
+			message: 'Gallery record created successfully',
+		});
+	} catch(e) {
+		console.error(e);
+		return res.status(500).json('Internal server error');
+	}
+}
+
+export const updateGalleryRecord = async (req: Request, res: Response): Promise<Response> => {
+	try {
+		const id = parseInt(req.params.id);
+		const { src, caption } = req.body;
+		await pool.query(`UPDATE gallery SET src='${src}', caption='${caption}' WHERE id=${id}`)
+		return res.status(200).json(`Gallery record ${id} updated successfully`);
+	} catch(e) {
+		console.error(e);
+		return res.status(500).json('Internal server error');
+	}
+}
+
+export const deleteGalleryRecord = async (req: Request, res: Response): Promise<Response> => {
+	try {
+		const id = parseInt(req.params.id);
+		await pool.query('DELETE FROM gallery where id = $1', [id])
+		return res.status(200).json(`Gallery record ${id} deleted successfully`);		
 	} catch(e) {
 		console.error(e);
 		return res.status(500).json('Internal server error');
--- spomenik-node/src/routes/index.ts
+++ spomenik-node/src/routes/index.ts
@@ -1,5 +1,6 @@
 import {Router} from 'express'
 import {getLetters, getLetterById, createLetter, updateLetter, deleteLetter} from '../controllers/index.controller'
+import {getGallery, createGalleryRecord, updateGalleryRecord, deleteGalleryRecord} from '../controllers/index.controller'
 
 const router = Router()
 
@@ -8,5 +9,10 @@ router.get('/letters/:id', getLetterById)
 router.post('/letters', createLetter)
 router.put('/letters/:id', updateLetter)
 router.delete('/letters/:id', deleteLetter)
+
+router.get('/gallery', getGallery)
+router.post('/gallery', createGalleryRecord)
+router.put('/gallery/:id', updateGalleryRecord)
+router.delete('/gallery/:id', deleteGalleryRecord)
 
 export default router
