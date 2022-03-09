842a78e2c4439b5a9299c3a3e44678f1d8ca0dcf
Milena Petkanova <milenapetkanova@gmail.com>
2022-03-09 17:21:59 +0200

    create gallery page and compoents

---
+++ spomenik-nuxt/components/gallery/GalleryList.vue
@@ -0,0 +1,70 @@
+<template>
+	<section class="gallery-list py-8">
+		<ul class="gallery-list__images">
+			<li 
+			  class="gallery-list__image"
+				v-for="(image, index) in getGallery" :key="index"
+			>
+				<img
+					class="gallery-list__image"
+					:src="image.thumb"
+					@click="openGallery(index)"
+				>
+			</li>
+		</ul>
+		<client-only v-if="isLoaded">
+			<light-box     
+				class="gallery-list__lightbox"   
+				ref="lightbox"
+				:media="getGallery"         
+				:show-caption="true"
+				:show-light-box="false" 
+			/>
+		</client-only>
+	</section>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  data() {
+    return {
+			index: null,
+			isLoaded: false,
+    }
+  },
+	computed:{
+    ...mapGetters('gallery', [
+      'getGallery',
+    ])
+  },
+	async mounted() {
+    await this.fetchGallery();
+  },
+	methods: {
+		...mapActions('gallery', ['initGallery']),
+		async fetchGallery() {
+      try {
+        const gallery = await this.$axios.$get('http://localhost:8000/gallery')
+        this.initGallery(gallery)
+				this.isLoaded = true
+      } catch (error) {
+        console.error(error)
+      }
+    },
+		openGallery(index) {
+      this.$refs.lightbox.showImage(index)
+    }
+	}
+}
+</script>
+
+<style scoped lang="scss">
+.gallery-list {
+  &__images {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(22rem, 1fr));
+    grid-gap: 2rem;
+  }
+}
+</style>
--- spomenik-nuxt/pages/gallery.vue
+++ spomenik-nuxt/pages/gallery.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="page is-gallery">
+    <div class="page__conatiner container py-8">
+      <section class="flex justify-end">
+        <button class="btn" @click="showModal = true">Добавете</button>
+    		<Modal v-show="showModal" @close-modal="showModal = false" >
+          <GalleryUpload @close-modal="showModal = false" />
+        </Modal>
+      </section>
+      
+      <GalleryList />
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({
+<script>
+export default{
   name: 'GalleryPage',
   head() {
     return {
-      title: 'Gallery Title',
+      title: 'Gallery',
     };
-  }
-})
+  },
+  data() {
+		return {
+			showModal: false,
+		}
+	},
+}
 </script>
