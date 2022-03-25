bc758b558a4ad9b26766214692225a5a65be6f08
Milena Petkanova <milenapetkanova@gmail.com>
2022-03-25 23:57:13 +0200

    create 500 and 404 error pages

--- spomenik-nuxt/plugins/axios.js
+++ spomenik-nuxt/plugins/axios.js
@@ -3,7 +3,12 @@ export default function({ store, app: { $axios }, redirect }) {
     if (store.state.auth.accessToken) {
       config.headers.Authorization = 'Bearer ' + store.state.auth.accessToken;
     }
-
     return config;
   });
+
+  $axios.onError(error => {
+    if (error.response.status === 500) {
+      redirect('/errors/500')
+    }
+  })
 }
